212ba3c0f1508e5791f2e6bbd2409f7b0cc35800
Kushagra Kumar <jakraso201@gmail.com>
2020-04-04 01:16:31 +0530

    Update Lifecycle

--- Android-Component/Activity/ActivityLifecycle/Lifecycle/app/src/main/java/com/example/lifecycle/MainActivity.java
+++ Android-Component/Activity/ActivityLifecycle/Lifecycle/app/src/main/java/com/example/lifecycle/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,41 +14,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"Inside onCreate");
+        Toast.makeText(this,"onCreate()",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.d(TAG,"Inside onStart");
+        Toast.makeText(this,"onStart()",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.d(TAG,"Inside onResume");
+        Toast.makeText(this,"onResume()",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.d(TAG,"Inside onPause");
+        Toast.makeText(this,"onPause",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.d(TAG,"Inside onStop");
+        Toast.makeText(this,"onStop()",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.d(TAG,"Inside onRestart");
+        Toast.makeText(this,"onRestart()",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG,"Inside onDestroy");
+        Toast.makeText(this,"onDestroy()",Toast.LENGTH_SHORT).show();
     }
+
+
 }
